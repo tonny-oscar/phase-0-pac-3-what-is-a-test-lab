0dbe6401a3098755605546b112e72304d79edfb7
Tonny Bett <betttonny26@gmail.com>
2024-06-27 21:07:59 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";
+const height = 39;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+describe("Height", () => {
+    it("is less than 40 and greater than 0", () => {
+      expect(height).toBeMoreThan(0)
+      expect(height).toBeLessThan(40);
+    });
+  });
